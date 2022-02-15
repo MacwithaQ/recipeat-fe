e2292854ac8679d11d27ed791461929dbf394a67
MacwithaQ <maqallaf@gmail.com>
2022-02-15 20:32:23 +0300

    home page

---
+++ src/categoriesStore.js
@@ -0,0 +1,13 @@
+import { makeAutoObservable } from "mobx";
+import categories from "./categoriesData";
+
+class CategoriesStore {
+  Category = categories;
+
+  constructor() {
+    makeAutoObservable(this);
+  }
+}
+
+const categoriesStore = new CategoriesStore();
+export default categoriesStore;
---
+++ src/components/NavBar.js
@@ -0,0 +1,58 @@
+import react from "react";
+import { NavLink } from "react-bootstrap";
+import {
+  Nav,
+  Navbar,
+  NavDropdown,
+  Form,
+  FormControl,
+  Button,
+  Container,
+  Image,
+} from "react-bootstrap";
+import frenchFries from "../media/FrenchFries.png";
+
+function NavBar() {
+  return (
+    <Navbar bg="dark" expand="lg" variant="dark">
+      <Container fluid>
+        <Navbar.Brand href="#" className="d-fl">
+          <Image src={frenchFries} style={{ width: 25, marginTop: -7 }} />
+          <h1>RecipEat</h1>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: "100px" }}
+            navbarScroll
+          >
+            <Nav.Link href="#action1">Home</Nav.Link>
+            <Nav.Link href="#action2">Categories</Nav.Link>
+            <NavDropdown title="Recipes" id="navbarScrollingDropdown">
+              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action4">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action5">
+                Something else here
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Form className="d-flex">
+            <FormControl
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavBar;
