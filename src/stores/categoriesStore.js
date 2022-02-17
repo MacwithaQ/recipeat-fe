7918c5b60449d6cbd7e2b8b95a0406b132561dfa
aziz <azizalghanim98@gmail.com>
2022-02-17 21:03:46 +0300

    categoriesStore BE

--- src/stores/categoriesStore.js
+++ src/stores/categoriesStore.js
@@ -13,15 +13,19 @@ class CategoriesStore {
     try {
       const res = await instance.get("/api/categories");
       this.categories = res.data;
-      console.log(this.categories)
     } catch (error) {
-      console.log("file: categoriesStore.js ~ line 18 ~",error);
+      console.log("file: categoriesStore.js ~ line 18 ~", error);
     }
   };
 
-  createCategory = (category) => {
-    console.log(category);
-    this.categories = [...this.categories, category];
+  createCategory = async (category) => {
+    try {
+      const res = await instance.post("/api/categories", category);
+      console.log(res)
+      this.categories = [...this.categories, res.data];
+    } catch (error) {
+      console.log(error);
+    }
   };
 }
 
